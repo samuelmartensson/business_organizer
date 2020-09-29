b5755cc6c4f2ab449f50d5fd1a9fee1ac5daf3ad
Samuel Mårtensson <samuel137@live.se>
2020-09-29 14:33:20 +0200

    Updates

---
+++ precache-manifest.492a4766c1bd7f71d46873c63e041fbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cb932bc16ae725a500708dde54af793",
+    "url": "/business_organizer/index.html"
+  },
+  {
+    "revision": "cac15ea8fd847b61d938",
+    "url": "/business_organizer/static/css/main.59e12669.chunk.css"
+  },
+  {
+    "revision": "272b2391ed861671796d",
+    "url": "/business_organizer/static/js/2.d0f3d2b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/business_organizer/static/js/2.d0f3d2b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cac15ea8fd847b61d938",
+    "url": "/business_organizer/static/js/main.486b80c4.chunk.js"
+  },
+  {
+    "revision": "a842927a704ea749fa82",
+    "url": "/business_organizer/static/js/runtime-main.ff8d4d37.js"
+  }
+]);
